d4418b958631278af65e71f5812a52a486a7955f
Aliaksei Syrel <alex.syrel@gmail.com>
2022-03-21 13:48:09 +0100

    Allow users to exit run_return on mac if there is more that one window

--- src/platform_impl/macos/app_state.rs
+++ src/platform_impl/macos/app_state.rs
@@ -406,14 +406,7 @@ impl AppState {
                 let windows: id = msg_send![app, windows];
                 let window_count: usize = msg_send![windows, count];
 
-                let dialog_open = if window_count > 1 {
-                    let dialog: id = msg_send![windows, lastObject];
-                    let is_main_window: BOOL = msg_send![dialog, isMainWindow];
-                    let is_visible: BOOL = msg_send![dialog, isVisible];
-                    is_visible != NO && is_main_window == NO
-                } else {
-                    false
-                };
+                let dialog_open = false;
 
                 let dialog_is_closing = HANDLER.dialog_is_closing.load(Ordering::SeqCst);
                 autoreleasepool(|| {
